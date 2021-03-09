aad0d1f9447313f73405116f847cce91521dbff8
Albert29-01 <aledu61@hotmail.com>
2021-03-09 19:04:33 -0300

    Compartiendo tabla

---
+++ src/database/migrations/Dump20210309.sql
@@ -0,0 +1,231 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: playandjoy
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'CAFETERA-1615042124861.jpg',2,'2021-03-06 14:48:45','2021-03-06 14:48:45',NULL),(2,'CAFETERA-1615043193194.jpg',3,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(3,'chocolate-1615043284845.jpg',4,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(4,'chocolate-1615043390671.jpg',5,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(5,'celular-1615043592513.jpg',6,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(6,'taza-1615043938451.jpg',7,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(7,'Tostadora-1615133311799.jpg',9,'2021-03-07 16:08:32','2021-03-07 16:08:32',NULL);
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `precio` decimal(12,2) NOT NULL,
+  `detalle` varchar(200) NOT NULL,
+  `info_ad` varchar(200) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `stock` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'televisor',123.00,'hola','Hola como estas',0,0,'2021-03-05 23:27:50','2021-03-05 23:27:50',NULL),(2,'CAFETERA',12345.00,'HOLA','HOLA COMO ESTAS ALBERT',0,0,'2021-03-06 14:48:44','2021-03-06 14:48:44',NULL),(3,'CAFETERA',123456.00,'hola','acá estamos entrando en panico',0,0,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(4,'chocolate',4568.00,'chau','otra vez probando',0,0,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(5,'chocolate',4568.00,'chau de nuevo','otra vez y van...',0,0,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(6,'celular',456.00,'como estan','volviendonos locos',0,0,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(7,'taza',59.00,'ya no damos mas','Se nos quemaron los papeles',0,0,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(8,'Tostadora',150.00,'Esto es una tostadora economica','No le pidas mucho más que no te queme las tostadas',0,0,'2021-03-07 16:01:08','2021-03-07 16:01:08',NULL),(9,'Tostadora',150.00,'Esto es una tostadora economica','Luce como un perro pero es una tostadora',0,0,'2021-03-07 16:08:31','2021-03-07 16:08:31',NULL);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suscripciones`
+--
+
+DROP TABLE IF EXISTS `suscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `suscripciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `precio` decimal(12,2) NOT NULL,
+  `detalle` varchar(200) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suscripciones`
+--
+
+LOCK TABLES `suscripciones` WRITE;
+/*!40000 ALTER TABLE `suscripciones` DISABLE KEYS */;
+INSERT INTO `suscripciones` VALUES (1,'',150585.00,'jdashjdashjdfn ',0,'2021-03-06 16:13:22','2021-03-06 16:13:22',NULL);
+/*!40000 ALTER TABLE `suscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `birth_date` date NOT NULL,
+  `domicilio` varchar(100) NOT NULL,
+  `localidad` varchar(100) NOT NULL,
+  `provincia` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas_productos`
+--
+
+DROP TABLE IF EXISTS `ventas_productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ventas_productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ventas_productos`
+--
+
+LOCK TABLES `ventas_productos` WRITE;
+/*!40000 ALTER TABLE `ventas_productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas_productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas_suscripciones`
+--
+
+DROP TABLE IF EXISTS `ventas_suscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ventas_suscripciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_suscripcion` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ventas_suscripciones`
+--
+
+LOCK TABLES `ventas_suscripciones` WRITE;
+/*!40000 ALTER TABLE `ventas_suscripciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas_suscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-09 19:02:08
