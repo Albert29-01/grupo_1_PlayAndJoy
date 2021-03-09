5bd35d8cc70a037a6707a7c437ab7a6a83038348
Albert29-01 <aledu61@hotmail.com>
2021-03-09 19:19:29 -0300

    export table

--- src/database/migrations/Dump20210309.sql
+++ src/database/migrations/Dump20210309.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `playandjoy` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `playandjoy`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: playandjoy
@@ -228,4 +230,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-09 19:02:08
+-- Dump completed on 2021-03-09 19:16:51
