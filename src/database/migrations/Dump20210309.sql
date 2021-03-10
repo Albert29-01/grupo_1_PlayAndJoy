0888e957a52f1952e8681465a9ebae184569ddc5
honeybadger2788 <noe.carosella@gmail.com>
2021-03-10 19:40:30 -0300

    actualizando base de datos

--- src/database/migrations/Dump20210309.sql
+++ src/database/migrations/Dump20210309.sql
@@ -27,11 +27,8 @@ DROP TABLE IF EXISTS `categorias`;
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +37,7 @@ CREATE TABLE `categorias` (
 
 LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'Vino'),(2,'Café'),(3,'Música'),(4,'Libro');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +56,7 @@ CREATE TABLE `images` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +65,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,'CAFETERA-1615042124861.jpg',2,'2021-03-06 14:48:45','2021-03-06 14:48:45',NULL),(2,'CAFETERA-1615043193194.jpg',3,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(3,'chocolate-1615043284845.jpg',4,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(4,'chocolate-1615043390671.jpg',5,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(5,'celular-1615043592513.jpg',6,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(6,'taza-1615043938451.jpg',7,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(7,'Tostadora-1615133311799.jpg',9,'2021-03-07 16:08:32','2021-03-07 16:08:32',NULL);
+INSERT INTO `images` VALUES (1,'CAFETERA-1615333520253.jpg',2,'2021-03-06 14:48:45','2021-03-09 23:45:20',NULL),(2,'CAFETERA-1615043193194.jpg',3,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(3,'chocolate-1615043284845.jpg',4,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(4,'chocolate-1615043390671.jpg',5,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(5,'celular-1615043592513.jpg',6,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(6,'taza-1615043938451.jpg',7,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(7,'Tostadora-1615133311799.jpg',9,'2021-03-07 16:08:32','2021-03-07 16:08:32',NULL),(8,'vaso de vidrio-1615330252184.jpg',10,'2021-03-09 22:50:52','2021-03-09 22:50:52',NULL),(9,'termo stanley-1615331164604.jpg',11,'2021-03-09 23:06:04','2021-03-09 23:06:04',NULL);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +88,7 @@ CREATE TABLE `productos` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +97,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'televisor',123.00,'hola','Hola como estas',0,0,'2021-03-05 23:27:50','2021-03-05 23:27:50',NULL),(2,'CAFETERA',12345.00,'HOLA','HOLA COMO ESTAS ALBERT',0,0,'2021-03-06 14:48:44','2021-03-06 14:48:44',NULL),(3,'CAFETERA',123456.00,'hola','acá estamos entrando en panico',0,0,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(4,'chocolate',4568.00,'chau','otra vez probando',0,0,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(5,'chocolate',4568.00,'chau de nuevo','otra vez y van...',0,0,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(6,'celular',456.00,'como estan','volviendonos locos',0,0,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(7,'taza',59.00,'ya no damos mas','Se nos quemaron los papeles',0,0,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(8,'Tostadora',150.00,'Esto es una tostadora economica','No le pidas mucho más que no te queme las tostadas',0,0,'2021-03-07 16:01:08','2021-03-07 16:01:08',NULL),(9,'Tostadora',150.00,'Esto es una tostadora economica','Luce como un perro pero es una tostadora',0,0,'2021-03-07 16:08:31','2021-03-07 16:08:31',NULL);
+INSERT INTO `productos` VALUES (1,'televisor',123.00,'hola','Hola como estas',0,0,'2021-03-05 23:27:50','2021-03-05 23:27:50',NULL),(2,'CAFETERA',54321.00,'texto editado','otro texto editado',0,10,'2021-03-06 14:48:44','2021-03-09 23:45:20',NULL),(3,'CAFETERA',123456.00,'hola','acá estamos entrando en panico',0,0,'2021-03-06 15:06:33','2021-03-06 15:06:33',NULL),(4,'chocolate',4568.00,'chau','otra vez probando',0,0,'2021-03-06 15:08:04','2021-03-06 15:08:04',NULL),(5,'chocolate',4568.00,'chau de nuevo','otra vez y van...',0,0,'2021-03-06 15:09:50','2021-03-06 15:09:50',NULL),(6,'celular',456.00,'como estan','volviendonos locos',0,0,'2021-03-06 15:13:12','2021-03-06 15:13:12',NULL),(7,'taza',59.00,'ya no damos mas','Se nos quemaron los papeles',0,0,'2021-03-06 15:18:58','2021-03-06 15:18:58',NULL),(8,'Tostadora',150.00,'Esto es una tostadora economica','No le pidas mucho más que no te queme las tostadas',0,0,'2021-03-07 16:01:08','2021-03-07 16:01:08',NULL),(9,'Tostadora',150.00,'Esto es una tostadora economica','Luce como un perro pero es una tostadora',0,0,'2021-03-07 16:08:31','2021-03-07 16:08:31',NULL),(10,'vaso de vidrio',1000000.00,'es un vaso de cristal','aunq parece un perro',1,0,'2021-03-09 22:50:52','2021-03-09 23:31:32','2021-03-09 23:31:32'),(11,'termo stanley',5000000.00,'con este medio termo, te sacamos un riñon','yafu\'',2,1,'2021-03-09 23:06:04','2021-03-09 23:29:50','2021-03-09 23:29:50');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +113,6 @@ CREATE TABLE `suscripciones` (
   `nombre` varchar(100) NOT NULL,
   `precio` decimal(12,2) NOT NULL,
   `detalle` varchar(200) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
@@ -129,7 +126,7 @@ CREATE TABLE `suscripciones` (
 
 LOCK TABLES `suscripciones` WRITE;
 /*!40000 ALTER TABLE `suscripciones` DISABLE KEYS */;
-INSERT INTO `suscripciones` VALUES (1,'',150585.00,'jdashjdashjdfn ',0,'2021-03-06 16:13:22','2021-03-06 16:13:22',NULL);
+INSERT INTO `suscripciones` VALUES (1,'',150585.00,'jdashjdashjdfn ','2021-03-06 16:13:22','2021-03-06 16:13:22',NULL);
 /*!40000 ALTER TABLE `suscripciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +152,7 @@ CREATE TABLE `usuarios` (
   `deleted_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,6 +161,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Noelia','Carosella','undefined-1615335674848.jpg','noe.carosella@gmail.com','$2a$12$j7QmkfuwBC18Lzg8ImrLF.lAqqrWXazaRcTkIFiA.MBJzqdh.0oJm','1988-07-27','Siempreviva 123','capital federal','Ciudad Autónoma de Buenos Aires','2021-03-10 00:21:15',NULL,'2021-03-10 00:21:15'),(2,'Sirius','Perrito','undefined-1615335789431.jpg','siri@gmail.com','$2a$12$hhJIsNnEF6Rl8Sf9c/8u.ufnbS7EsMjxwvH8Ny4/J2OOMvtnnf9jS','2020-12-28','Siempreviva 123','capital federal','Ciudad Autónoma de Buenos Aires','2021-03-10 00:23:09',NULL,'2021-03-10 00:23:09'),(3,'Otro','Sirius','undefined-1615335918092.jpg','otroSirius@gmail.com','$2a$12$6n.9sEsJe/GzPNqoRTTUYuO4QTU1w0CPr79yfJRnDqHWajSHLyrl6','2020-12-28','Siempreviva 123','capital federal','Ciudad Autónoma de Buenos Aires','2021-03-10 00:25:18',NULL,'2021-03-10 00:25:18');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,4 +228,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-09 19:16:51
+-- Dump completed on 2021-03-10 19:40:07
