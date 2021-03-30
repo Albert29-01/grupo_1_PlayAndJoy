7c8bf7e20aae1281d370bf46620dc358fbc389b8
honeybadger2788 <noe.carosella@gmail.com>
2021-03-29 21:39:59 -0300

    modificando vistas en general

--- public/js/registerjs.js
+++ public/js/registerjs.js
@@ -1,5 +1,6 @@
-function myFunction(event){
+function selectProvincia(event){
     let provinciaID = document.querySelector('option.provincia').value
+    console.log('ID Provincia',provinciaID)
     fetch('https://apis.datos.gob.ar/georef/api/municipios?provincia='+provinciaID+'&campos=id,nombre&max=100')
     .then(function(response){
         return response.json()
@@ -8,6 +9,6 @@ function myFunction(event){
         for (let i = 0; i < data.municipios.length; i++) {
            document.querySelector('select.municipio').innerHTML += '<option value='+data.municipios[i].id+'>'+data.municipios[i].nombre+'</option>' 
         }
-        console.log(data)
+        console.log('Data Municipios',data)
     })
 }
