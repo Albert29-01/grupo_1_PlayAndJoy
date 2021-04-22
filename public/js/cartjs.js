8aa31e9cc23715560d39c8da9aa5c668e5ffdc1f
honeybadger2788 <noe.carosella@gmail.com>
2021-04-22 19:59:44 -0300

    corrigiendo carrito

--- public/js/cartjs.js
+++ public/js/cartjs.js
@@ -1,10 +1,10 @@
-    if (localStorage == undefined || localStorage.carrito == undefined || localStorage.carrito == "[]") { //ver validacion
+    if (localStorage == undefined || localStorage.carrito == undefined || localStorage.carrito == "[]") { 
         document.querySelector('p.cartEmpty').innerHTML = 'No hay productos agregados al carrito'
     } else {
         let arrayProductos = JSON.parse(localStorage.carrito)
         for (let i = 0; i < arrayProductos.length; i++) {
             let precio = arrayProductos[i].precioProducto.slice(1)   //para sacarle el símbolo pesos al precio
-            document.querySelector('tbody').innerHTML += '<tr class="producto"><td class="first"><button style="border: none" class="deleteButton" onclick=deleteItem('+i+')><img src="/img/basura.png" alt="eliminar item"></button></td><td>'+arrayProductos[i].nombreProducto+'</td><td><input type="number" value='+arrayProductos[i].cantProducto+'></td><td>$'+precio*arrayProductos[i].cantProducto+'</td></tr>'
+            document.querySelector('tbody').innerHTML += '<tr class="producto"><td class="first"><button style="border: none" class="deleteButton" onclick=deleteItem('+i+')><img src="/img/basura.png" alt="eliminar item"></button></td><td>'+arrayProductos[i].nombreProducto+'</td><td><input type="number" id="cantidad'+i+'" onchange=addItem('+i+') value='+arrayProductos[i].cantProducto+'></td><td id="precio'+i+'">$'+precio*arrayProductos[i].cantProducto+'</td></tr>'
         }
     }
 
@@ -41,6 +41,18 @@
         localStorage.setItem('carrito',JSON.stringify(carrito));
         console.log(carrito)
         location.reload();
+    }
+
+    function addItem(id){
+        let carrito = JSON.parse(localStorage.carrito)
+        for (let i = 0; i < carrito.length; i++) {
+            if (i == id) {
+                let precio = carrito[i].precioProducto.slice(1)
+                let cantidad = document.getElementById('cantidad'+i).value 
+                precio = precio*cantidad
+                document.getElementById('precio'+i).innerHTML = '$ '+precio
+            }
+        }
     }
     
     
